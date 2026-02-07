fc58342e27ecc3b8f25b88c59d464e5b93a87b66
PCMadumere <madumerepreciouschukwuka@gmail.com>
2026-02-07 10:42:07 +0100

    form validation completed

--- Form Validation/script.js
+++ Form Validation/script.js
@@ -42,7 +42,42 @@ function validatePhone(){
 function validateEmail(){
     var email = document.getElementById("contact-email").value;
 
-    
+    if(email.length == 0){
+        emailError.innerHTML == "Email is required"
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = "Invalid Email"
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateMessage(){
+    var message = document.getElementById("contact-message").value;
+    var required = 30;
+    var left = required - message.length;
+
+    if(left > 0){
+        messageError.innerHTML = left + " more characters required";
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fa-sharp fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm(){
+    if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+        submitError.style.display = "block";
+        submitError.innerHTML = "Please fix error to submit";
+        setTimeout(function(){
+            submitError.style.display = "none";
+        }, 3000);
+        return false;
+    }
 }
 
 
